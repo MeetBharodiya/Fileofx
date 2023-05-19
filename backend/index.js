68a66bf54f9010a1202f53c2cde39ecfd28bb4c3
meet <meetbharodiya2002@gmail.com>
2023-05-19 10:03:13 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,25 @@ const port = 5000;
 
 app.use(cors());
 
+app.all("*", function (req, res, next) {
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type,Authorization ,Accept"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Expose-Headers", "Authorization");
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type, Authorization"
+  );
+  next();
+});
+
 connectToMongo();
 
 //middelware req.body mate
